258a88f9ea45ba19594b604cc55f568e367064ae
Ivan <js314head@gmail.com>
2020-10-20 19:46:57 +0200

    Updates

---
+++ precache-manifest.3069740c9467c03a1623c5e97306c22e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b237d3dd10892bcdcb52d00720882e6b",
+    "url": "/spnft/index.html"
+  },
+  {
+    "revision": "056f82b5dbb9ff29ab7d",
+    "url": "/spnft/static/css/main.1b0ccdcc.chunk.css"
+  },
+  {
+    "revision": "290557c2ba3ac7013cc2",
+    "url": "/spnft/static/js/2.73a8a90c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/spnft/static/js/2.73a8a90c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056f82b5dbb9ff29ab7d",
+    "url": "/spnft/static/js/main.91d51902.chunk.js"
+  },
+  {
+    "revision": "1a73b42aca7323d15542",
+    "url": "/spnft/static/js/runtime-main.8b1162b8.js"
+  },
+  {
+    "revision": "61e754be8e302ec32ac27cede363a832",
+    "url": "/spnft/static/media/logo.61e754be.png"
+  }
+]);
